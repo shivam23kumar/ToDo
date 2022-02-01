c240cf9951f74d9dfc380fae89e0823cd02959b0
shivam23kumar <shivamkumar.dev23@gmail.com>
2022-02-01 11:27:02 +0530

    js(done)

--- todo.js
+++ todo.js
@@ -1,13 +1,31 @@
-let tasks = [];
+(function(){
+    let tasks = [];
+    const tasksList = document.getElementById('list');
+    const addTaskInput = document.getElementById('add');
+    const tasksCounter = document.getElementById('tasks-counter');
 
-const taskList = document.getElementById('list');
-const addTaskInput = document.getElementById('list');
-const taskCounter = document.getElementById('list');
+function addTaskToDOM(task) {
+    const li = document.createElement('li');
 
+    li.innerHTML = `
+        <input type="checkbox" id="${task.id}" ${task.done ? 'checked' : ''} class="custom-checkbox">
+        <label for="${task.id}">${task.text}</label>
+        <img src="bin.svg" class="delete" data-id="${task.id}" />
+    `;
+    tasksList.append(li);
+}
 
-function renderList(){};
+function renderList(){
+    tasksList.innerHTML = '';
 
-function markTaskAsComplete(taskId){
+    for(let i =0; i<tasks.length; i++){
+        addTaskToDOM(tasks[i]);
+    }
+    tasksCounter.innerHTML = tasks.length;
+
+};
+
+function toggleTask(taskId){
     const task = tasks.filter(function(task){
         task.id === taskId;
     })
@@ -24,8 +42,8 @@ function markTaskAsComplete(taskId){
 
 function deleteTask(taskId) {
     const newTasks = tasks.filter(function(task){
-        task.id !== taskId;
-    })
+        return task.id !== taskId;
+    });
     tasks = newTasks;
     renderList();
     showNotification('Task deleted successfully');
@@ -50,12 +68,12 @@ function handleInputKeypress(e){
     if(e.key === 'Enter'){
         const text = e.target.value;
         if(!text){
-            showNotification("Task text can't be empty");
+            showNotification('Task text can not be empty');
             return;
         }
         const task = {
             text,
-            id = Date.now().toString(),
+            id: Date.now().toString(),
             done:false
         }
         e.target.value='';
@@ -65,4 +83,27 @@ function handleInputKeypress(e){
     
 }
 
-addTaskInput.addEventListener('keyup', handleInputKeypress);
+function handleClickListener(e) {
+    const target = e.target;
+    if(target.className === 'delete'){
+        const taskId = target.dataset.id;
+        deleteTask(taskId);
+        return;
+    }
+    else if(target.className === 'custom-checkbox'){
+        const taskId = target.id;
+        toggleTask(taskId);
+        return;
+    }
+}
+
+
+function intialiseApp() {
+    
+    addTaskInput.addEventListener('keyup', handleInputKeypress);
+
+    document.addEventListener('click', handleClickListener);
+}
+intialiseApp();
+
+})();
